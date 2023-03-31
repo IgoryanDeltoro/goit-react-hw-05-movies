ac0536dbcc30ad4bee9e446d72cf06581902d941
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-03-31 20:27:59 +0300

    width of header was fixed

--- src/components/sharedLayout/SharedLayout.jsx
+++ src/components/sharedLayout/SharedLayout.jsx
@@ -1,16 +1,25 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Container, Header, Logo, Link, Loading } from './SharedLayout.styled';
+import {
+  Container,
+  Header,
+  Logo,
+  Link,
+  Loading,
+  HeaderBox,
+} from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
     <>
       <Header>
-        <Logo />
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="movies">Movies</Link>
-        </nav>
+        <HeaderBox>
+          <Logo />
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="movies">Movies</Link>
+          </nav>
+        </HeaderBox>
       </Header>
       <Container>
         <Suspense fallback={<Loading>Loading page...</Loading>}>
